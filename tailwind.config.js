73f20015744f7acdec66e8122fdfd7e24a7e85f4
Thomas Eduardo <developer.thomas@outlook.com.br>
2024-11-12 13:37:53 -0300

    add config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	prefix: 'tw-',
-	important: false,
-	content: [
-		"**/*.{html, jsx, js}",
-		"**/*.js",
-		"**/*.html",
-	],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			 
-			fontFamily: {
-				poly: ['"poly"', "serif"],
-			},
-		},
-	},
-	plugins: [
-		function({ addVariant }) {
-			addVariant('firefox', ':-moz-any(&)')
-		}
-	],
-}
-
+  prefix: 'tw-', // Prefixo 'tw-' para todas as classes do Tailwind
+  important: false, // Pode ser verdadeiro caso queira dar maior prioridade aos estilos do Tailwind
+  darkMode: 'class', // Ativa o modo escuro com uma classe 'dark'
+  content: [
+    './**/*.{html,js,jsx}', // Certifica-se de que o Tailwind irá processar os arquivos corretos
+    // Adicione outros diretórios ou arquivos conforme necessário, ex: './src/**/*.{html,js,jsx}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poly: ['"poly"', 'serif'], // Personalizando a fonte
+      },
+    },
+  },
+  plugins: [
+    function ({ addVariant }) {
+      // Adiciona variante customizada para o Firefox
+      addVariant('firefox', ':-moz-any(&)');
+    },
+  ],
+};
